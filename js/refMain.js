89e2d9e6a439c83559606f7c46005d7a09035951
Joe <JoeDBNS@users.noreply.github.com>
2018-05-01 15:10:25 -0400

    Rearranged and Updated
    Rearranged order to fit standard portfolio site layout. Added Job section that looks better than last idea.

--- js/refMain.js
+++ js/refMain.js
@@ -50,31 +50,119 @@ Project: mega-project-hub
 					features: [
 						"Vue.js",
 						"Firebase",
-						"SimpleGrid"
+						"SimpleGrid",
+						"Responsive Design"
+					]
+				},
+				{
+					name: "StepForward Michigan",
+					folder: 'projects/StepForward/index.html',
+					color: '',
+					features: [
+						"JavaScript",
+						"Responsive Design",
+						"Formatted Printing"
+					]
+				},
+				{
+					name: "TinyMCE Letters",
+					folder: 'projects/TinyMCE/index.html',
+					color: '',
+					features: [
+						"JavaScript",
+						"TinyMCE",
+						"Formatted Printing"
+					]
+				},
+				{
+					name: "ColorVoter",
+					folder: 'projects/ColorVotes/index.html',
+					color: '',
+					features: [
+						"JavaScript",
+						"Firebase",
+						"Game"
 					]
 				}
 			],
-			Works: [
+			Jobs: [
 				{
 					employer: "State of Michigan",
 					department: 'DTMB - Agency Services',
+					title: 'Web and Software Developer',
 					dateBegin: 'Mar 2017',
 					dateEnd: 'Present',
-					title: 'Web and Software Developer'
+					length: '13+',
+					style: {
+						transform: GetTransformValue(),
+						top: '26px',
+						left: '40px'
+					}
 				},
 				{
 					employer: "Eaton County",
 					department: 'Technology Services',
+					title: 'Computer Technician Intern',
 					dateBegin: 'Oct 2016',
 					dateEnd: 'Oct 2017',
-					title: 'Computer Technician Intern'
+					length: '12',
+					style: {
+						transform: GetTransformValue(),
+						top: '230px',
+						left: '110px'
+					}
 				},
 				{
 					employer: "State of Michigan",
 					department: 'DTMB - Design & Delivery',
+					title: 'IT Student Assistant',
 					dateBegin: 'Nov 2015',
 					dateEnd: 'Mar 2017',
-					title: 'IT Student Assistant'
+					length: '17',
+					style: {
+						transform: GetTransformValue(),
+						top: '54px',
+						left: '210px'
+					}
+				},
+				{
+					employer: "Best Buy",
+					department: 'Computer Dept.',
+					title: 'Sales Consultant',
+					dateBegin: 'Mar 2015',
+					dateEnd: 'Jun 2016',
+					length: '16',
+					style: {
+						transform: GetTransformValue(),
+						top: '216px',
+						left: '310px'
+					}
+				},
+				{
+					employer: "Advanced Rheumatology",
+					department: 'IT Support',
+					title: 'Computer and Data Technician',
+					dateBegin: '___ 201_',
+					dateEnd: '___ 201_',
+					length: '__',
+					style: {
+						transform: GetTransformValue(),
+						top: '30px',
+						left: '400px'
+					}
+				},
+				{
+					employer: "Charlotte High School",
+					department: 'IT Dept.',
+					title: 'Computer Technician Intern',
+					dateBegin: '___ 201_',
+					dateEnd: '___ 201_',
+					length: '__',
+					style: {
+						transform: GetTransformValue(),
+						top: '240px',
+						left: '484px'
+					}
 				}
 			]
 		},
@@ -83,21 +171,11 @@ Project: mega-project-hub
 				this.session.showComputerScreen = 1;
 				this.session.selectedProject = project;
 			},
-			WorkSelect: function(work) {
-				$('.work').each(function() {
-					$(this).removeClass('workSelected')
+			JobClicked: function(job) {
+				swal({
+					title: job.title,
+					text: job.employer
 				});
-				if ($(event.path[0]).hasClass('work')) {
-					$(event.path[0]).addClass('workSelected');
-				}
-				else if (event.srcElement.localName == 'p') {
-					$(event.path[1]).addClass('workSelected');
-				}
-				else if (event.srcElement.localName == 'b') {
-					$(event.path[2]).addClass('workSelected');
-				}
-				this.session.selectedWork = work;
-				this.session.showFolderInfo = 1;
 			}
 		},
 		computed: {
@@ -118,4 +196,14 @@ Project: mega-project-hub
 		var date = new Date();
 		var dateFormat = date.getMonth().toString() + "-" + (date.getDay() + 1).toString() + "-" + date.getFullYear().toString();
 		return dateFormat;
+	}
+	function GetTransformValue() {
+		var nVal = Math.floor((Math.random() * 2) + 1);
+		var tVal = Math.floor((Math.random() * 6) + 1);
+		if (nVal == 1) {
+			var tString = "rotate(-" + tVal + "deg)"
+		}
+		else
+			var tString = "rotate(" + tVal + "deg)"
+		return tString;
 	}
